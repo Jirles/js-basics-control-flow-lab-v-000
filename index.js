b5e17433a05038b7b3baab18409f5e1b52d69907
Emily Jirles <emily.jirles@gmail.com>
2018-07-10 15:13:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(rideDistance){
   if (rideDistance <= 400) {
-    'This one is one me!'
+    'This one is one me!';
   } else if (rideDistance > 2000){
-    'I will gladly take your thiry bucks.'
+    'I will gladly take your thiry bucks.';
   } else if (rideDistance > 2500) {
-    'No can do.'
+    'No can do.';
   }
 };
